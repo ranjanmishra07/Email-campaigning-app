f7d64b56aa654e52c3384e88ec6d65d3c22cd39b
Ranjan Mishra <mishra.ranjan5@gmail.com>
2018-03-05 23:34:46 +0530

    updated config

---
+++ config/keys.js
@@ -0,0 +1,11 @@
+module.exports={
+  mongoURI:'mongodb://raja:raja@ds115768.mlab.com:15768/nerdtalk',
+  githubClientID: 'b531e27ee51666c46ada',
+  githubClientSecret: '4c0430897fc6b6a82ae288e2dc315225822c7180',
+
+  googleClientID      : '228396924815-8bu8ui0c4a7jope2cv8vsblv152hdoav.apps.googleusercontent.com',
+  googleClientSecret  : 'L36VQx6HqqU4Kr5dMKFkWFt9',
+  cookieKey:'ratialgroup07',
+  stripeSecretKey:'sk_test_d8GF6ZINy3ZTcnnj7JwwOleZ'
+
+}
