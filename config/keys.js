95a0a933b8c6aa2314c21062269dd95c7513d94c
Ranjan Mishra <mishra.ranjan5@gmail.com>
2018-03-06 00:17:04 +0530

    updated keys

--- config/keys.js
+++ config/keys.js
@@ -1,11 +1,9 @@
-module.exports={
-  mongoURI:'mongodb://raja:raja@ds115768.mlab.com:15768/nerdtalk',
-  githubClientID: 'b531e27ee51666c46ada',
-  githubClientSecret: '4c0430897fc6b6a82ae288e2dc315225822c7180',
 
-  googleClientID      : '228396924815-8bu8ui0c4a7jope2cv8vsblv152hdoav.apps.googleusercontent.com',
-  googleClientSecret  : 'L36VQx6HqqU4Kr5dMKFkWFt9',
-  cookieKey:'ratialgroup07',
-  stripeSecretKey:'sk_test_d8GF6ZINy3ZTcnnj7JwwOleZ'
-
+// keys.js - figure out what set of credentials to return
+if (process.env.NODE_ENV === 'production') {
+  // we are in production - return the prod set of keys
+  module.exports = require('./prod');
+} else {
+  // we are in development - return the dev keys!!!
+  module.exports = require('./dev');
 }
---
+++ config/prod.js
@@ -0,0 +1,9 @@
+module.exports = {
+  googleClientID: process.env.GOOGLE_CLIENT_ID,
+  googleClientSecret: process.env.GOOGLE_CLIENT_SECRET,
+  mongoURI: process.env.MONGO_URI,
+  cookieKey: process.env.COOKIE_KEY,
+  stripePublishableKey: 'pk_test_hy5DqgGQ8npUFSWpf8AkhauH',
+  stripeSecretKey: 'sk_test_d8GF6ZINy3ZTcnnj7JwwOleZ',
+
+};
