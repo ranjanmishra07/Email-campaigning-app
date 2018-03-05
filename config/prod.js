440dbc7bbe944247c3bbacf1dcdd0a0b993a4b81
Ranjan Mishra <mishra.ranjan5@gmail.com>
2018-03-06 00:21:47 +0530

    updated idf

--- config/prod.js
+++ config/prod.js
@@ -1,6 +1,8 @@
 module.exports = {
   googleClientID: process.env.GOOGLE_CLIENT_ID,
   googleClientSecret: process.env.GOOGLE_CLIENT_SECRET,
+  githubClientID: 'b531e27ee51666c46ada',
+  githubClientSecret: '4c0430897fc6b6a82ae288e2dc315225822c7180',
   mongoURI: process.env.MONGO_URI,
   cookieKey: process.env.COOKIE_KEY,
   stripePublishableKey: 'pk_test_hy5DqgGQ8npUFSWpf8AkhauH',
